852466b7bddf972115a73e6dceb8e712c9046549
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-05-30 19:46:14 -0300

    refactor: agrega versión anterior de EditTaskPage para revisión

--- frontend/src/routes/EditTaskPage.tsx
+++ frontend/src/routes/EditTaskPage.tsx
@@ -1,23 +1,69 @@
 import { useNavigate, useParams } from "react-router-dom";
+import type { Task } from "../types/types";
+import { useEffect, useState } from "react";
+import { getTask } from "../services/api";
 import TaskForm from "../components/ui/TaskForm";
-import { useEditTaskPage } from "../hooks/useEditTaskPage";
 
 function EditTaskPage() {
   const { id } = useParams();
+  const [task, setTask] = useState<Task | null>(null);
   const navigate = useNavigate();
-  const { task, setTask } = useEditTaskPage(id || "");
+
+  useEffect(() => {
+    if (id) {
+      getTask(id).then(setTask).catch(console.error);
+    }
+  }, [id]);
+
+  useEffect(() => {
+    if (id) {
+      getTask(id)
+        .then(setTask)
+        .catch(() => alert("Tarea no encontrada"));
+    }
+  }, [id]);
+
+  const handleSave = (updatedTask: Task) => {
+    setTask(updatedTask);
+    navigate("/");
+  };
+
+  if (!task) return <p>Cargando...</p>;
 
   return (
     <main className="container">
-      <TaskForm
-        task={task || undefined}
-        onSave={(updatedTask) => {
-          setTask(updatedTask);
-          navigate(`/task/${updatedTask.id}`);
-        }}
-      />
+      <article className="flex flex-col items-center py-4 w-full">
+        <header className="py-4">
+          <h1 className="text-3xl mb-4 font-semibold">Editar tarea</h1>
+        </header>
+        <TaskForm task={task} onSave={handleSave} />
+      </article>
     </main>
   );
 }
 
 export default EditTaskPage;
+
+// import { useNavigate, useParams } from "react-router-dom";
+// import TaskForm from "../components/ui/TaskForm";
+// import { useEditTaskPage } from "../hooks/useEditTaskPage";
+
+// function EditTaskPage() {
+//   const { id } = useParams();
+//   const navigate = useNavigate();
+//   const { task, setTask } = useEditTaskPage(id || "");
+
+//   return (
+//     <main className="container">
+//       <TaskForm
+//         task={task || undefined}
+//         onSave={(updatedTask) => {
+//           setTask(updatedTask);
+//           navigate(`/task/${updatedTask.id}`);
+//         }}
+//       />
+//     </main>
+//   );
+// }
+
+// export default EditTaskPage;
